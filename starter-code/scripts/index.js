80c3977df5a3006a4a8e074e8c9e8bcc62b7889b
Pleasant Yomi <110311997+pleasantyomi@users.noreply.github.com>
2022-10-29 15:43:59 +0000

    Initial commit

---
+++ starter-code/scripts/index.js
@@ -0,0 +1,4 @@
+const open = () =>{
+    // document.querySelector('.sidenav').classList.toggle('open');
+    alert('We are the best')
+}
---
+++ starter-code/scripts/onclick.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x=
+    document.getElementById("my_sidenav");
+    if (x.style.display === "none") {
+        x.style.display= "block";
+    } else {
+        x.style.display= "none";
+    }
+}
+function myFunction2() {
+    var x=
+    document.getElementById("my_sidenav");
+    if (x.style.display === "none") {
+        x.style.display= "block";
+    } else {
+        x.style.display= "none";
+    }
+}
